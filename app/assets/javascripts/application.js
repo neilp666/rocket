f090f57b4f79166a398585bf1c9fa839c9090313
neil patel <neil@myindianweddingapp.com>
2016-04-02 21:46:19 +0100

    make rocket move

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,17 +22,35 @@ var countdownNumber = 10;
 var changeState = function (state) {
   document.body.className = 'body-state' + state;
   clearInterval(timer);
+  countdownNumber = 10;
+  document.getElementById('countdown').innerHTML = countdownNumber;
 
+  //countdown
   if (state == 2) {
     timer = setInterval(function () {
-       document.getElementById('countdown').innerHTML = countdownNumber;
+  
        countdownNumber = countdownNumber -1;
-       if (countdownNumber <= 0) {
-        
-        changeState(3);
-               };
-    },1000);
-  } else if (state ==3) {
 
+      document.getElementById('countdown').innerHTML = countdownNumber;
+
+       if (countdownNumber <= 0) {
+           changeState(3);
+         };
+    }, 500);
+
+  } else if (state == 3) {
+      var success = setTimeout(function ()
+      {
+        var randomNumber = Math.round(Math.random()*10);
+
+        // Success
+
+        if (randomNumber > 1) {
+          changeState(4);
+
+        } else {
+          changeState(5);  // oh no!
+        }
+      }, 2000);
   };
 }
