a3cd11160f88bccb5d9a46bbb97531bdd7ba59a2
neil patel <neil@myindianweddingapp.com>
2016-04-02 21:25:53 +0100

    Added countdown sequence

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,24 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var timer = null;
+var countdownNumber = 10;
+
+var changeState = function (state) {
+  document.body.className = 'body-state' + state;
+  clearInterval(timer);
+
+  if (state == 2) {
+    timer = setInterval(function () {
+       document.getElementById('countdown').innerHTML = countdownNumber;
+       countdownNumber = countdownNumber -1;
+       if (countdownNumber <= 0) {
+        
+        changeState(3);
+               };
+    },1000);
+  } else if (state ==3) {
+
+  };
+}
